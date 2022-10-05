d4416bb079d627d8dc62b67f39cf11ac931362ba
Lime007 <ljinmyung@kakao.com>
2022-10-06 03:56:41 +0900

    게시판 완성!!

--- src/main/java/com/example/layout/controller/BoardController.java
+++ src/main/java/com/example/layout/controller/BoardController.java
@@ -1,13 +1,15 @@
 package com.example.layout.controller;
 
-import com.example.layout.model.board;
+import com.example.layout.model.Board;
 import com.example.layout.repository.BoardRepository;
+import com.example.layout.validator.BoardValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -17,10 +19,35 @@ public class BoardController {
     @Autowired
     private BoardRepository boardRepository;
 
+    @Autowired
+    private BoardValidator boardValidator;
+
     @GetMapping("/list")
     public String list(Model model){
-        List<board> boards = boardRepository.findAll();
+        List<Board> boards = boardRepository.findAll();
         model.addAttribute("boards", boards);
         return "board/list";
+    }
+
+    @GetMapping("/form")
+    public String form(Model model, @RequestParam(required = false) Long id){
+        if(id == null){
+            model.addAttribute("board", new Board());
+        } else {
+            Board board = boardRepository.findById(id).orElse(null);
+            model.addAttribute("board", board);
+        }
+
+        return "board/form";
+    }
+
+    @PostMapping("/form")
+    public String greetingSubmit(@Valid Board board, BindingResult bindingResult) {
+        boardValidator.validate(board, bindingResult);
+        if(bindingResult.hasErrors()){
+            return "board/form";
+        }
+        boardRepository.save(board);
+        return "redirect:/board/list";
     }
 }
--- src/main/java/com/example/layout/repository/BoardRepository.java
+++ src/main/java/com/example/layout/repository/BoardRepository.java
@@ -1,10 +1,10 @@
 package com.example.layout.repository;
 
-import com.example.layout.model.board;
+import com.example.layout.model.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface BoardRepository extends JpaRepository<board, Long> {
-    List<board> findAll();
+public interface BoardRepository extends JpaRepository<Board, Long> {
+    List<Board> findAll();
 }
